641d0ef86db830b2d0dde9d7ece66d367013edc5
alibaba0010 <zakariyahali100@gmail.com>
2024-11-06 09:52:16 +0100

    working with app.jsx, navber

---
+++ client/src/components/Navbar/NavbarItem.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const NavbarItem = ({ title, classProps }) => {
+  return <li className={`mx-4 cursor-pointer ${classProps}`}>{title}</li>;
+};
+
+NavbarItem.propTypes = {
+  title: PropTypes.string.isRequired,
+  classProps: PropTypes.string.isRequired,
+};
+export default NavbarItem;
