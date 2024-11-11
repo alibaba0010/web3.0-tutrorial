4d66f70bdb58fcc1082b51c98c9f1afd8943c0f5
alibaba0010 <zakariyahali100@gmail.com>
2024-11-11 01:37:29 +0100

    Adding dummmy data to working with the transaction component, adding custom hook to work with the giphy api

---
+++ client/src/hooks/useFetch.js
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+
+const API_KEY = import.meta.VITE_GIPHY_API_KEY;
+
+const useFetch = ({ keyword }) => {
+  const [data, setData] = useState("");
+
+  const fetchData = async () => {
+    try {
+      console.log("In fetch::::");
+      const response = await fetch(
+        `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${keyword
+          .split(" ")
+          .join("")}&limit=1`
+      );
+      const result = await response.json();
+      console.log("Result: " + result);
+      setData(result.data[0]?.images?.downsized_medium.url);
+    } catch (e) {
+      setData(
+        "https://metro.co.uk/wp-content/uploads/2015/05/pokemon_crying.gif?quality=90&strip=all&zoom=1&resize=500%2C284"
+      );
+    }
+  };
+  useEffect(() => {
+    if (keyword) {
+      fetchData();
+    }
+  }, [keyword]);
+
+  return data;
+};
+
+export default useFetch;
